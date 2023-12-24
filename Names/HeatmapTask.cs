b42a11482d73f260a51ce6a1c987fd6895b20993
Danila Zakharov <danila.zaxarov.2015@gmail.com>
2023-12-24 14:42:17 +0300

    add solution for heatmap task

--- Names/HeatmapTask.cs
+++ Names/HeatmapTask.cs
@@ -1,13 +1,30 @@
-﻿namespace Names;
+﻿using System.Collections.Generic;
 
-internal static class HeatmapTask
+namespace Names
 {
-    public static HeatmapData GetBirthsPerDateHeatmap(NameData[] names)
+    internal static class HeatmapTask
     {
-        return new HeatmapData(
-            "Пример карты интенсивностей",
-            new double[,] { { 1, 2, 3 }, { 2, 3, 4 }, { 3, 4, 4 }, { 4, 4, 4 } }, 
-            new[] { "a", "b", "c", "d" }, 
-            new[] { "X", "Y", "Z" });
+        public static HeatmapData GetBirthsPerDateHeatmap(IEnumerable<NameData> names)
+        {
+            var days = new string[30];
+            for (var i = 0; i < days.Length; i++)            
+                days[i] = (i + 2).ToString();
+            
+            var months = new string[12];
+            for (var i = 0; i < months.Length; i++)
+                months[i] = (i + 1).ToString();
+
+            var colors = new double[days.Length, months.Length];            
+
+            foreach (var name in names)
+                if (name.BirthDate.Day != 1)
+                    colors[name.BirthDate.Day - 2, name.BirthDate.Month - 1]++;
+
+            return new HeatmapData(                
+                "Пример карты интенсивностей",
+                colors,
+                days,
+                months);
+        }
     }
 }
