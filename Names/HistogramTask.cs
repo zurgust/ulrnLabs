403c7e9649a11357603dde6a7668ed739ad5176e
Danila Zakharov <danila.zaxarov.2015@gmail.com>
2023-12-24 14:11:55 +0300

    add all files with solution HistogramTask

---
+++ Names/HistogramTask.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace Names
+{
+    internal static class HistogramTask
+    {
+        public static HistogramData GetBirthsPerDayHistogram(NameData[] names, string name)
+        {
+            double[] days = new double[32];
+            string[] daysStr = new string[31];
+            
+            foreach (var n in names)
+                if (n.Name == name && n.BirthDate.Day != 1) days[n.BirthDate.Day]++;
+            
+            for (int i = 0; i < daysStr.Length; i++)
+                daysStr[i] = (i+1).ToString();
+
+            return new HistogramData(
+                string.Format("Рождаемость людей с именем '{0}'", name), 
+                daysStr, 
+                days.Skip(1).ToArray());
+        }
+    }
+}
+
